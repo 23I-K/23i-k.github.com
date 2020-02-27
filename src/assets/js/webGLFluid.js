a269215021ee9f4c7a28a9f0d86bbfe3c701d9de
MØ <prostokurtki@gmail.com>
2020-02-27 18:22:40 +0300

    WebGlFx + deviceOrientation

--- src/assets/js/webGLFluid.js
+++ src/assets/js/webGLFluid.js
@@ -1,4 +1,4 @@
-export function WebGl() {
+export function WebGl(externalConfig) {
   /*
   MIT License
 
@@ -26,36 +26,17 @@ export function WebGl() {
   const canvas = document.getElementsByTagName("canvas")[0];
   resizeCanvas();
 
-  let config = {
-    SIM_RESOLUTION: 64,
-    DYE_RESOLUTION: 256,
-    CAPTURE_RESOLUTION: 512,
-    DENSITY_DISSIPATION: 2,
-    VELOCITY_DISSIPATION: 3,
-    PRESSURE: 0.8,
-    PRESSURE_ITERATIONS: 20,
-    CURL: 12,
-    SPLAT_RADIUS: 0.4,
-    SPLAT_FORCE: 6000,
-    SHADING: true,
-    COLORFUL: true,
-    COLOR_UPDATE_SPEED: 10,
-    PAUSED: false,
-    BACK_COLOR: {r: 23, g: 25, b: 65},
-    TRANSPARENT: true,
-    TRANSPARENT_WITH_CHECKERBOARD: false,
-    BLOOM: true,
-    BLOOM_ITERATIONS: 8,
-    BLOOM_RESOLUTION: 256,
-    BLOOM_INTENSITY: 0.4,
-    BLOOM_THRESHOLD: 0.2,
-    BLOOM_SOFT_KNEE: 0.7,
-    SUNRAYS: false,
-    SUNRAYS_RESOLUTION: 196,
-    SUNRAYS_WEIGHT: 1.0,
-    ACTIVATE_AT_HOVER: true,
-    INIT_SPLATS_ON_START: false
+  let config = externalConfig;
+
+  window.mobileAndTabletcheck = function() {
+    let check = false;
+    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+    return check;
   };
+
+  if (window.mobileAndTabletcheck()) {
+    config.DISABLE_TOUCH_MOVE = true;
+  }
 
   function pointerPrototype() {
     this.id = -1;
@@ -1577,47 +1558,73 @@ export function WebGl() {
     updatePointerUpData(pointers[0]);
   });
 
-  canvas.addEventListener("touchstart", e => {
-    e.preventDefault();
-    const touches = e.targetTouches;
-    while (touches.length >= pointers.length)
-      pointers.push(new pointerPrototype());
-    for (let i = 0; i < touches.length; i++) {
-      let posX = scaleByPixelRatio(touches[i].pageX);
-      let posY = scaleByPixelRatio(touches[i].pageY);
-      updatePointerDownData(pointers[i + 1], touches[i].identifier, posX, posY);
-    }
-  });
+  if (!config.DISABLE_TOUCH_MOVE) {
 
-  canvas.addEventListener(
-    "touchmove",
-    e => {
+    canvas.addEventListener("touchstart", e => {
       e.preventDefault();
       const touches = e.targetTouches;
+      while (touches.length >= pointers.length)
+        pointers.push(new pointerPrototype());
       for (let i = 0; i < touches.length; i++) {
-        let pointer = pointers[i + 1];
-        if (!pointer.down) continue;
         let posX = scaleByPixelRatio(touches[i].pageX);
         let posY = scaleByPixelRatio(touches[i].pageY);
-        updatePointerMoveData(pointer, posX, posY);
+        updatePointerDownData(pointers[i + 1], touches[i].identifier, posX, posY);
       }
-    },
-    false
-  );
+    });
 
-  window.addEventListener("touchend", e => {
-    const touches = e.changedTouches;
-    for (let i = 0; i < touches.length; i++) {
-      let pointer = pointers.find(p => p.id == touches[i].identifier);
-      if (pointer == null) continue;
-      updatePointerUpData(pointer);
-    }
-  });
+    canvas.addEventListener(
+      "touchmove",
+      e => {
+        e.preventDefault();
+        const touches = e.targetTouches;
+        for (let i = 0; i < touches.length; i++) {
+          let pointer = pointers[i + 1];
+          if (!pointer.down) continue;
+          let posX = scaleByPixelRatio(touches[i].pageX);
+          let posY = scaleByPixelRatio(touches[i].pageY);
+          updatePointerMoveData(pointer, posX, posY);
+        }
+      },
+      false
+    );
+
+    window.addEventListener("touchend", e => {
+      const touches = e.changedTouches;
+      for (let i = 0; i < touches.length; i++) {
+        let pointer = pointers.find(p => p.id == touches[i].identifier);
+        if (pointer == null) continue;
+        updatePointerUpData(pointer);
+      }
+    });
+  }
 
   window.addEventListener("keydown", e => {
     if (e.code === "KeyP") config.PAUSED = !config.PAUSED;
     if (e.key === " ") splatStack.push(parseInt(Math.random() * 20) + 5);
   });
+
+  if (config.ENABLE_ORIENTATION_MOVE) {
+
+    if (window.DeviceOrientationEvent) {
+      window.addEventListener("deviceorientation", function(event) {
+        let rotateDegrees = event.alpha;
+        let leftToRight = event.gamma;
+        let frontToBack = event.beta;
+
+        handleOrientationEvent(frontToBack, leftToRight, rotateDegrees);
+      }, true);
+    }
+
+    let handleOrientationEvent = function(frontToBack, leftToRight, rotateDegrees) {
+      let pointer = pointers[0];
+      if (!config.ACTIVATE_AT_HOVER) {
+        if (!pointer.down) return;
+      }
+      let posX = scaleByPixelRatio(rotateDegrees*3);
+      let posY = scaleByPixelRatio(frontToBack*3);
+      updatePointerMoveData(pointer, posX, posY);
+    };
+  }
 
   function updatePointerDownData(pointer, id, posX, posY) {
     pointer.id = id;
--- src/components/PageHeader/PageHeader.jsx
+++ src/components/PageHeader/PageHeader.jsx
@@ -20,10 +20,46 @@ import {WebGl} from 'assets/js/webGLFluid';
 import {Container} from "reactstrap";
 
 class PageHeader extends React.Component {
-
+  constructor(props) {
+    super(props);
+    this.webGlFluidConfig = {
+      SIM_RESOLUTION: 64,
+      DYE_RESOLUTION: 256,
+      CAPTURE_RESOLUTION: 512,
+      DENSITY_DISSIPATION: 2,
+      VELOCITY_DISSIPATION: 3,
+      PRESSURE: 0.8,
+      PRESSURE_ITERATIONS: 20,
+      CURL: 12,
+      SPLAT_RADIUS: 0.4,
+      SPLAT_FORCE: 6000,
+      SHADING: true,
+      COLORFUL: true,
+      COLOR_UPDATE_SPEED: 10,
+      PAUSED: false,
+      BACK_COLOR: {r: 23, g: 25, b: 65},
+      TRANSPARENT: true,
+      TRANSPARENT_WITH_CHECKERBOARD: false,
+      BLOOM: true,
+      BLOOM_ITERATIONS: 8,
+      BLOOM_RESOLUTION: 256,
+      BLOOM_INTENSITY: 0.4,
+      BLOOM_THRESHOLD: 0.2,
+      BLOOM_SOFT_KNEE: 0.7,
+      SUNRAYS: false,
+      SUNRAYS_RESOLUTION: 196,
+      SUNRAYS_WEIGHT: 1.0,
+      ACTIVATE_AT_HOVER: true,
+      INIT_SPLATS_ON_START: false,
+      DISABLE_TOUCH_MOVE: false,
+      ENABLE_ORIENTATION_MOVE: false
+    };
+  }
   componentDidMount() {
-    this.webGlInstance = new WebGl();
-    // this.webGlInstance.insertImageToCenter(require("assets/img/neonShalaver.png"));
+    if (window.DeviceOrientationEvent) {
+      this.webGlFluidConfig.ENABLE_ORIENTATION_MOVE = true;
+    }
+    this.webGlInstance = new WebGl(this.webGlFluidConfig);
   }
 
   render() {
@@ -31,7 +67,7 @@ class PageHeader extends React.Component {
       <div className="page-header header-filter">
         <canvas/>
         <Container>
-          <div className="content-center brand content-center-top-fix">
+          <div className="content-center brand">
             <img src={require("assets/img/neonShalaver.png")} alt={'shaverLover neon logo'}/>
           </div>
         </Container>
